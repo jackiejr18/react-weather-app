e0ad7b6acbe341e453948684b08e2bd0b3a1a12e
Jackie Jr <jwysinger@gmail.com>
2024-04-12 16:28:07 -0500

    removal of decimal places and completion of app

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -32,8 +32,8 @@ const WeatherApp = () => {
     const location = document.getElementsByClassName('weather-location');
 
     humidity[0].innerHTML = data.main.humidity + '%';
-    wind[0].innerHTML =  data.wind.speed + ' mph'; 
-    temperature[0].innerHTML = data.main.temp + '°F'; 
+    wind[0].innerHTML = Math.floor(data.wind.speed) + ' mph'; 
+    temperature[0].innerHTML = Math.floor(data.main.temp) + '°F'; 
     location[0].innerHTML = data.name; 
 
     if(data.weather[0].icon==='01d' || data.weather[0].icon==='01n') {
